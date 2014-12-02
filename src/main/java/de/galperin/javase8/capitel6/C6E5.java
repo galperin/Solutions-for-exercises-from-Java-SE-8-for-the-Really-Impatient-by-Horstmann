e2322898baecbbe535b498c13e8236fd107378b6
Eugen Galperin <eugen.galperin@gmail.com>
2014-12-02 09:10:26 +0100

    new exercises

---
+++ src/main/java/de/galperin/javase8/capitel6/C6E5.java
@@ -0,0 +1,50 @@
+package de.galperin.javase8.capitel6;
+
+import de.galperin.javase8.Exercise;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * User: eugen
+ * Date: 02.12.14
+ */
+public class C6E5 implements Exercise {
+
+    @Test
+    @Override
+    public void perform() {
+        ConcurrentHashMap<String, Set<File>> map = new ConcurrentHashMap<>();
+        File[] files = {
+                new File(this.getClass().getResource("/alice.txt").getFile()),
+                new File(this.getClass().getResource("/person.fxml").getFile()),
+        };
+        Arrays.asList(files).parallelStream().forEach(f -> {
+            try {
+                Arrays.asList(new String(Files.readAllBytes(f.toPath()), StandardCharsets.UTF_8)
+                        .split("[\\P{L}]+")).stream().forEach(w -> {
+                    Set<File> set = new HashSet<>();
+                    set.add(f);
+                    map.merge(w, set, (existingValue, newValue) -> {
+                        existingValue.addAll(newValue);
+                        return existingValue;
+                    });
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        assertNotNull(map.get("name"));
+        assertEquals(2, map.get("name").size());
+    }
+}
