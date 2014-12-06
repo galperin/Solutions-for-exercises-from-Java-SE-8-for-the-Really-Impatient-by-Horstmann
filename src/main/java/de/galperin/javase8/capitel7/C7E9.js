9eee21352a0637b1d901cb4992e3109fb0a44142
Eugen Galperin <eugen.galperin@gmail.com>
2014-12-06 10:27:58 +0100

    new exercises

---
+++ src/main/java/de/galperin/javase8/capitel7/C7E9.js
@@ -0,0 +1,10 @@
+var age = $ENV.AGE;
+if($ARG.length > 0){
+    age = $ARG[0];
+}
+if(!age || age != parseInt(age, 10)){
+    do {
+        age = readLine('Your age: ');
+    } while(age != parseInt(age, 10))
+}
+print("Next year you will be ${++age}");
