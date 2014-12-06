e78019bb8c99a3293c622a5ea8daa52fc16707a4
Eugen Galperin <eugen.galperin@gmail.com>
2014-12-06 16:16:44 +0100

    new exercises

---
+++ src/main/java/de/galperin/javase8/capitel8/C8E2.java
@@ -0,0 +1,17 @@
+package de.galperin.javase8.capitel8;
+
+import de.galperin.javase8.Exercise;
+import org.junit.Test;
+
+/**
+ * User: eugen
+ * Date: 06.12.14
+ */
+public class C8E2 implements Exercise {
+
+    @Test(expected = Exception.class)
+    @Override
+    public void perform() {
+        Math.negateExact(Integer.MIN_VALUE);
+    }
+}
