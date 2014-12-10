e163bbe2164cb357088d4d5a6b5465cf3e206728
Eugen Galperin <eugen.galperin@gmail.com>
2014-12-10 08:35:29 +0100

    new exercises

---
+++ src/main/java/de/galperin/javase8/capitel8/C8E10.java
@@ -0,0 +1,95 @@
+package de.galperin.javase8.capitel8;
+
+import de.galperin.javase8.Exercise;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Enumeration;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * User: eugen
+ * Date: 09.12.14
+ */
+public class C8E10 implements Exercise {
+
+    @Test
+    @Override
+    public void perform() {
+        try {
+            String srcPath = System.getenv().get("JAVA_HOME") + "/src.zip";
+            String tempPath = System.getProperty("java.io.tmpdir") + "/c8e10";
+            extract(srcPath, tempPath);
+            try (Stream<Path> entries = Files.walk(Paths.get(tempPath))) {
+                 entries.filter(p -> !Files.isDirectory(p)).forEach(p -> {
+                     try {
+                         if (Files.lines(p).anyMatch(s -> s.contains("transient") || s.contains("volatile"))) {
+                             System.out.printf("%s%n", p);
+                         }
+                     } catch (IOException e) {
+                         e.printStackTrace();
+                     }
+                 });
+            }
+            clean(tempPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    private void extract(String zipFile, String extractFolder) {
+        try {
+            int BUFFER = 2048;
+            File file = new File(zipFile);
+            ZipFile zip = new ZipFile(file);
+            new File(extractFolder).mkdir();
+            Enumeration zipFileEntries = zip.entries();
+            while (zipFileEntries.hasMoreElements()) {
+                ZipEntry entry = (ZipEntry) zipFileEntries.nextElement();
+                String currentEntry = entry.getName();
+                File destFile = new File(extractFolder, currentEntry);
+                File destinationParent = destFile.getParentFile();
+                destinationParent.mkdirs();
+                if (!entry.isDirectory()) {
+                    try (BufferedInputStream is = new BufferedInputStream(zip.getInputStream(entry))) {
+                        int currentByte;
+                        byte data[] = new byte[BUFFER];
+                        FileOutputStream fos = new FileOutputStream(destFile);
+                        try (BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER)) {
+                            while ((currentByte = is.read(data, 0, BUFFER)) != -1) {
+                                dest.write(data, 0, currentByte);
+                            }
+                            dest.flush();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clean(String path) throws IOException {
+        Path directory = Paths.get(path);
+        Files.walkFileTree(directory, new SimpleFileVisitor<Path>() {
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+    }
+}
