ef391bef39686f4f26256850be0ce0da3facf200
Eugen Galperin <eugen.galperin@gmail.com>
2014-12-14 13:32:20 +0100

    new exercises

---
+++ src/main/java/de/galperin/javase8/C9E8.java
@@ -0,0 +1,50 @@
+package de.galperin.javase8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * User: eugen
+ * Date: 14.12.14
+ */
+public class C9E8 implements Exercise {
+
+    @Test
+    @Override
+    public void perform() {
+        Point a = new Point(Integer.MAX_VALUE, 8);
+        Point b = new Point(-100, 8);
+        assertTrue(a.compareTo(b) > 0);
+        Point c = new Point(-Integer.MAX_VALUE, 8);
+        Point d = new Point(100, 8);
+        assertTrue(c.compareTo(d) < 0);
+    }
+
+    class Point implements Comparable<Point> {
+
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point other) {
+            int diff;
+            try {
+                diff = Math.subtractExact(x, other.x);
+            } catch (ArithmeticException e) {
+                return x;
+            }
+            if (diff != 0) return diff;
+            try {
+                return Math.subtractExact(y, other.y);
+            } catch (ArithmeticException e) {
+                return y;
+            }
+        }
+    }
+}
